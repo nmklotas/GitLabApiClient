7810af6ba1558b273e5a70de5ad8549dbf4da613
nmklotas <nemoku@gmail.com>
2017-09-03 12:30:52 +0300

    add issues querying by project

--- src/GitLabApiClient/IssuesClient.cs
+++ src/GitLabApiClient/IssuesClient.cs
@@ -6,32 +6,37 @@ using GitLabApiClient.Models.Issues;
 
 namespace GitLabApiClient
 {
-    public class IssuesClient
+    public sealed class IssuesClient
     {
         private readonly GitLabHttpFacade _httpFacade;
         private readonly IssuesQueryBuilder _queryBuilder;
+        private readonly ProjectIssuesQueryBuilder _projectIssuesQueryBuilder;
 
-        internal IssuesClient(GitLabHttpFacade httpFacade, IssuesQueryBuilder queryBuilder)
+        internal IssuesClient(
+            GitLabHttpFacade httpFacade, 
+            IssuesQueryBuilder queryBuilder,
+            ProjectIssuesQueryBuilder projectIssuesQueryBuilder)
         {
             _httpFacade = httpFacade;
             _queryBuilder = queryBuilder;
+            _projectIssuesQueryBuilder = projectIssuesQueryBuilder;
         }
 
         public async Task<Issue> GetAsync(int projectId, int issueId) => 
             await _httpFacade.Get<Issue>($"/projects/{projectId}/issues/{issueId}");
 
-        public async Task<IList<Issue>> GetAsync(Action<IssuesQueryOptions> options = null)
-        {
-            var queryOptions = new IssuesQueryOptions();
-            options?.Invoke(queryOptions);
-
-            string url = _queryBuilder.Build("/issues", queryOptions);
-            return await _httpFacade.GetPagedList<Issue>(url);
-        }
-
         public async Task<IList<Issue>> GetAsync(string projectId, Action<ProjectIssuesQueryOptions> options = null)
         {
             var queryOptions = new ProjectIssuesQueryOptions(projectId);
+            options?.Invoke(queryOptions);
+
+            string url = _projectIssuesQueryBuilder.Build("/issues", queryOptions);
+            return await _httpFacade.GetPagedList<Issue>(url);
+        }
+
+        public async Task<IList<Issue>> GetAsync(Action<IssuesQueryOptions> options = null)
+        {
+            var queryOptions = new IssuesQueryOptions();
             options?.Invoke(queryOptions);
 
             string url = _queryBuilder.Build("/issues", queryOptions);
