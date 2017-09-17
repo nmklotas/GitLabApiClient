4bd1d77ad09a8d950b5f009387239de987fb31d7
nmklotas <nemoku@gmail.com>
2017-09-17 13:50:31 +0300

    Move build script to a file

---
+++ build.ps1
@@ -0,0 +1,40 @@
+function Build()
+{
+	& dotnet restore GitLabApiClient.sln | Write-Host
+	if ($LastExitCode -ne 0)
+	{
+		exit 1
+	}
+
+	& dotnet build src\GitLabApiClient\GitLabApiClient.csproj -c Release | Write-Host
+	if ($LastExitCode -ne 0)
+	{
+		exit 1
+	}
+}
+
+function Pack()
+{
+	if ($env:APPVEYOR_REPO_TAG_NAME) 
+	{
+		$version = $env:APPVEYOR_REPO_TAG_NAME
+		& dotnet pack src\GitLabApiClient\GitLabApiClient.csproj -c Release --version-suffix $version --no-build | Write-Host
+	}
+	else
+	{
+		& dotnet pack src\GitLabApiClient\GitLabApiClient.csproj -c Release --no-build | Write-Host
+	}
+		
+	if ($LastExitCode -ne 0)
+	{
+		exit 1
+	}
+}
+
+function Main()
+{
+	Build
+	Pack
+}
+
+Main
