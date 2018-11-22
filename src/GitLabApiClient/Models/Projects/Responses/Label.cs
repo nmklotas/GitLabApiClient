a7d1af069c9fff0aa52bcfac5318fc2fce11847b
DTeuchert <denny.teuchert@web.de>
2018-11-22 15:26:50 +0100

    Integrate gitlab api functions for project labels

---
+++ src/GitLabApiClient/Models/Projects/Responses/Label.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace GitLabApiClient.Models.Projects.Responses
+{
+    public sealed class Label
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("color")]
+        public string Color { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("priority")]
+        public int? Priority { get; set; }
+    }
+}
