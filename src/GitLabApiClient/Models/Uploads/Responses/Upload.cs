bc659f1fd4cda047b2aeed040d27f7c2ac55a5bd
yankun <yan.kun0567@gmail.com>
2019-04-23 20:30:47 +0200

    Upload files (#27)
    * Added methods to upload a file to gitlab for later use in markdown aware fields.
    
    * Fixed typo caused by ReSharpers refactoring.
    
    * Form-part name ist now set as required.
    
    * Moved projectId property to Request model to ensure that pattern is the same verywhere.

---
+++ src/GitLabApiClient/Models/Uploads/Responses/Upload.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace GitLabApiClient.Models.Uploads.Responses
+{
+    public sealed class Upload
+    {
+        [JsonConstructor]
+        public Upload(string alt, string url, string markdown)
+        {
+            Alt = alt;
+            Url = url;
+            Markdown = markdown;
+        }
+
+        public string Alt { get; }
+        public string Url { get; }
+        public string Markdown { get; }
+    }
+}
