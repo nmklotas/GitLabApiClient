f6ecab0d9532b86d5b72824aec79ca797ac19881
David Nagl <dnagl.dev@gmail.com>
2019-09-06 22:41:17 +0200

    Added documentation

---
+++ src/GitLabApiClient/Models/Releases/Requests/ReleaseQueryOptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GitLabApiClient.Models.Projects.Responses;
+
+namespace GitLabApiClient.Models.Releases.Requests
+{
+    public sealed class ReleaseQueryOptions
+    {
+        public string ProjectId { get; set; }
+        public string TagName { get; set; }
+
+        internal ReleaseQueryOptions(string projectId = null) => ProjectId = projectId;
+    }
+}
