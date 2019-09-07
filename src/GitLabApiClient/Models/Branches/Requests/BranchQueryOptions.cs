30f8ac7335c70a7bb590b457af0e9d89579b4fc6
David Nagl <dnagl.dev@gmail.com>
2019-09-07 20:03:31 +0200

    Added BranchQueryOptions class

---
+++ src/GitLabApiClient/Models/Branches/Requests/BranchQueryOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitLabApiClient.Models.Branches.Requests
+{
+    public sealed class BranchQueryOptions
+    {
+        public string ProjectId { get; set; }
+        public string BranchName { get; set; }
+
+        internal BranchQueryOptions(string projectId = null) => ProjectId = projectId;
+    }
+}
