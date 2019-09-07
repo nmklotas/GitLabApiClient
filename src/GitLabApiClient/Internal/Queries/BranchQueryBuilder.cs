0aaebe7770f7ef191cf98b491e9b3e95ec4d2651
David Nagl <dnagl.dev@gmail.com>
2019-09-07 20:04:35 +0200

    Added BranchQueryBuilder

---
+++ src/GitLabApiClient/Internal/Queries/BranchQueryBuilder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GitLabApiClient.Models.Branches.Requests;
+
+namespace GitLabApiClient.Internal.Queries
+{
+    internal class BranchQueryBuilder : QueryBuilder<BranchQueryOptions>
+    {
+        protected override void BuildCore(BranchQueryOptions options)
+        {
+            if (!string.IsNullOrEmpty(options.ProjectId))
+                Add("id", options.ProjectId);
+
+            if (!string.IsNullOrEmpty(options.BranchName))
+                Add("branch", options.BranchName);
+        }
+    }
+}
