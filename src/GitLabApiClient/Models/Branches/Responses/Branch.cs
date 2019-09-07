c8bd5530916ebe22e81bc401e317e8620c00d9a4
David Nagl <dnagl.dev@gmail.com>
2019-09-07 19:37:41 +0200

    Added Branch response

---
+++ src/GitLabApiClient/Models/Branches/Responses/Branch.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GitLabApiClient.Models.Releases.Responses;
+using Newtonsoft.Json;
+
+namespace GitLabApiClient.Models.Branches.Responses
+{
+    public sealed class Branch
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        
+        [JsonProperty("merged")]
+        public bool Merged { get; set; }
+
+        [JsonProperty("protected")]
+        public bool Protected { get; set; }
+
+        [JsonProperty("default")]
+        public bool Default { get; set; }
+
+        [JsonProperty("developers_can_push")]
+        public bool DevelopersCanPush { get; set; }
+
+        [JsonProperty("developers_can_merge")]
+        public bool DevelopersCanMerge { get; set; }
+
+        [JsonProperty("can_push")]
+        public bool CanPush { get; set; }
+
+        [JsonProperty("commit")]
+        public Commit Commit { get; set; }
+    }
+}
