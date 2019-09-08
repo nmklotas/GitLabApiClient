1a5b9eeee9a3709854ead2f0b2318fb9215bd210
David Nagl <dnagl.dev@gmail.com>
2019-09-08 11:32:51 +0200

    Feature/branches (#39)
    * Added Branch response
    
    * Added Request for creating a branch
    
    * Renamed CreateBranch class to CreateBranchRequest
    
    * Added Guard to constructor
    
    * Added DeleteBranchRequest class
    
    * Added DeleteMergedBranchesRequest
    
    * Made DeleteMergedBranchesRequest public sealed
    
    * Added BranchQueryOptions class
    
    * Added BranchQueryBuilder
    
    * Updated classname for CreateBranchRequest
    
    * Changed CreateBranchRequest Property 'Id' to 'ProjectId'
    
    * Changed property names for DeleteBranchRequest class
    
    * Changed property names for DeleteMergedBranchesRequest class
    
    * Added BranchClient
    
    * Fixed tests
    
    * Fixed XML cref warning

---
+++ src/GitLabApiClient/Models/Branches/Requests/CreateBranchRequest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GitLabApiClient.Internal.Utilities;
+using Newtonsoft.Json;
+
+namespace GitLabApiClient.Models.Branches.Requests
+{
+    /// <summary>
+    /// Creates a new branch on a project.
+    /// </summary>
+    public sealed class CreateBranchRequest
+    {
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CreateBranchRequest"/> class
+        /// </summary>
+        /// <param name="projectId">Id or URL-Encoded Path of the project.</param>
+        /// <param name="branch">Name of the Branch.</param>
+        /// <param name="reference">Branch name or commit SHA to create branch from.</param>
+        public CreateBranchRequest(string projectId, string branch, string reference)
+        {
+            Guard.NotEmpty(projectId, nameof(projectId));
+            Guard.NotEmpty(branch, nameof(branch));
+            Guard.NotEmpty(reference, nameof(reference));
+            ProjectId = projectId;
+            Branch = branch;
+            Reference = reference;
+        }
+
+        /// <summary>
+        /// Project ID.
+        /// </summary>
+        [JsonProperty("id")]
+        public string ProjectId { get; set; }
+
+        /// <summary>
+        /// Branch Name.
+        /// </summary>
+        [JsonProperty("branch")]
+        public string Branch { get; set; }
+
+        /// <summary>
+        /// Branch Reference.
+        /// </summary>
+        [JsonProperty("ref")]
+        public string Reference { get; set; }
+
+    }
+}
---
+++ src/GitLabApiClient/Models/Branches/Requests/DeleteBranchRequest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GitLabApiClient.Internal.Utilities;
+
+namespace GitLabApiClient.Models.Branches.Requests
+{
+    /// <summary>
+    /// Deletes a Branh from a project.
+    /// </summary>
+    public sealed class DeleteBranchRequest
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DeleteBranchRequest"/> class.
+        /// </summary>
+        /// <param name="id">Id or URL-Encoded path of the project</param>
+        /// <param name="branch">Name of the branch</param>
+        public DeleteBranchRequest(string id, string branch)
+        {
+            Guard.NotNullOrDefault(id, nameof(id));
+            Guard.NotEmpty(branch, nameof(branch));
+            ProjectId = id;
+            BranchName = branch;
+        }
+
+        /// <summary>
+        /// Project Id.
+        /// </summary>
+        public string ProjectId { get; set; }
+        
+        /// <summary>
+        /// Branch Name.
+        /// </summary>
+        public string BranchName { get; set; }
+    }
+}
