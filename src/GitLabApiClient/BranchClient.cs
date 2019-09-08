96426b67ba49ac3cef89ecf04bf7576d4b97b12d
David Nagl <dnagl.dev@gmail.com>
2019-09-08 11:32:51 +0200

    Feature/branches (#39)
    * Added Branch response
    
    * Added Request for creating a branch
    
    * Renamed CreateBranch class to CreateBranchRequest
    
    * Added Guard to constructor
    
    * Added DeleteBranchRequest class
    
    * Added DeleteMergedBranchesRequest
    
    * Made DeleteMergedBranchesRequest public sealed
    
    * Added BranchQueryOptions class
    
    * Added BranchQueryBuilder
    
    * Updated classname for CreateBranchRequest
    
    * Changed CreateBranchRequest Property 'Id' to 'ProjectId'
    
    * Changed property names for DeleteBranchRequest class
    
    * Changed property names for DeleteMergedBranchesRequest class
    
    * Added BranchClient
    
    * Fixed tests
    
    * Fixed XML cref warning

---
+++ src/GitLabApiClient/BranchClient.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using GitLabApiClient.Internal.Http;
+using GitLabApiClient.Internal.Queries;
+using GitLabApiClient.Models.Branches.Requests;
+using GitLabApiClient.Models.Branches.Responses;
+
+namespace GitLabApiClient
+{
+    public sealed class BranchClient
+    {
+        private readonly GitLabHttpFacade _httpFacade;
+        private readonly BranchQueryBuilder _branchQueryBuilder;
+
+        internal BranchClient(
+            GitLabHttpFacade httpFacade,
+            BranchQueryBuilder branchQueryBuilder)
+        {
+            _httpFacade = httpFacade;
+            _branchQueryBuilder = branchQueryBuilder;
+        }
+
+        public async Task<Branch> GetAsync(string projectId, string branchName) =>
+            await _httpFacade.Get<Branch>($"projects/{projectId}/branches/{branchName}");
+
+        public async Task<IList<Branch>> GetAsync(string projectId, Action<BranchQueryOptions> options)
+        {
+            var queryOptions = new BranchQueryOptions(projectId);
+            options?.Invoke(queryOptions);
+
+            string url = _branchQueryBuilder.Build($"projects/{projectId}/branches", queryOptions);
+            return await _httpFacade.GetPagedList<Branch>(url);
+        }
+
+        public async Task<Branch> CreateAsync(CreateBranchRequest request) =>
+            await _httpFacade.Post<Branch>($"projects/{request.ProjectId}/branches", request);
+
+        public async Task DeleteBranch(DeleteBranchRequest request) =>
+            await _httpFacade.Delete($"projects/{request.ProjectId}/releases/{request.BranchName}");
+
+        public async Task DeleteMergedBranches(DeleteMergedBranchesRequest request) =>
+            await _httpFacade.Delete($"projects/{request.ProjectId}/repository/merged_branches");
+
+    }
+}
