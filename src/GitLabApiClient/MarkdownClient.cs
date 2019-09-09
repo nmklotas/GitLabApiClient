5a4eb2a44b0dd2d153913baaf6345c736ae850a1
David Nagl <dnagl.dev@gmail.com>
2019-09-09 22:45:41 +0200

    Markdown Support (#45)
    * Added Markdown response
    
    * Added RenderMarkdownRequest
    
    * Added MarkdownClient
    
    * Fixed bool error for request

---
+++ src/GitLabApiClient/MarkdownClient.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using GitLabApiClient.Internal.Http;
+using GitLabApiClient.Models.Markdown.Request;
+using GitLabApiClient.Models.Markdown.Response;
+
+namespace GitLabApiClient
+{
+    /// <summary>
+    /// Used to render a markdown document.
+    /// </summary>
+    public sealed class MarkdownClient
+    {
+        private readonly GitLabHttpFacade _httpFacade;
+
+        internal MarkdownClient(GitLabHttpFacade httpFacade) => 
+            _httpFacade = httpFacade;
+
+        public async Task<Markdown> RenderAsync(RenderMarkdownRequest request) =>
+            await _httpFacade.Post<Markdown>($"api/v4/markdown", request);
+    }
+}
