8f6b4d209afadf108a816088f2f8431bf1cd4e0b
David Nagl <dnagl.dev@gmail.com>
2019-09-09 22:45:41 +0200

    Markdown Support (#45)
    * Added Markdown response
    
    * Added RenderMarkdownRequest
    
    * Added MarkdownClient
    
    * Fixed bool error for request

---
+++ src/GitLabApiClient/Models/Markdown/Response/Markdown.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace GitLabApiClient.Models.Markdown.Response
+{
+    public sealed class Markdown
+    {
+        [JsonProperty("html")]
+        public string Html { get; set; }
+    }
+}
