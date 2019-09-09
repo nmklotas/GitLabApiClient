9d479bc57896237288cce9805527c1d1126fa316
David Nagl <dnagl.dev@gmail.com>
2019-09-09 18:56:57 +0200

    Added TagQueryBuilder

---
+++ src/GitLabApiClient/Internal/Queries/TagQueryBuilder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GitLabApiClient.Models.Tags.Requests;
+
+namespace GitLabApiClient.Internal.Queries
+{
+    internal class TagQueryBuilder : QueryBuilder<TagQueryOptions>
+    {
+        protected override void BuildCore(TagQueryOptions options)
+        {
+            if (!string.IsNullOrEmpty(options.ProjectId))
+                Add("id", options.ProjectId);
+
+            if (!string.IsNullOrEmpty(options.TagName))
+                Add("tag_name", options.TagName);
+        }
+    }
+}
