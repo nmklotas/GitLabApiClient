e4b3d71430f7b014e9c2180da898ef689ee7b1c5
David Nagl <dnagl.dev@gmail.com>
2019-09-09 18:39:39 +0200

    Added query options for tags

---
+++ src/GitLabApiClient/Models/Tags/Requests/TagQueryOptsions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitLabApiClient.Models.Tags.Requests
+{
+    public sealed class TagQueryOptsions
+    {
+        public string ProjectId { get; set; }
+        public string TagName { get; set; }
+
+        internal TagQueryOptsions(string projectId = null) => ProjectId = projectId;
+    }
+}
