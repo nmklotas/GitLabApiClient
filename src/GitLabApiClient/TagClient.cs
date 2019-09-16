fa9198ab8797f620e55f457801bda52350955c06
MindaugasLaganeckas <MindaugasLaganeckas@users.noreply.github.com>
2019-09-16 15:06:52 +0200

    BUG FIX: repository keyword was missing in the request url (#51)

--- src/GitLabApiClient/TagClient.cs
+++ src/GitLabApiClient/TagClient.cs
@@ -23,22 +23,27 @@ namespace GitLabApiClient
         }
 
         public async Task<Tag> GetAsync(string projectId, string tagName) =>
-            await _httpFacade.Get<Tag>($"projects/{projectId}/repository/tags/{tagName}");
+            await _httpFacade.Get<Tag>(TagsBaseUrl(projectId) + "/{tagName}");
 
         public async Task<IList<Tag>> GetAsync(string projectId, Action<TagQueryOptions> options)
         {
             var queryOptions = new TagQueryOptions(projectId);
             options?.Invoke(queryOptions);
 
-            string url = _tagQueryBuilder.Build($"projects/{projectId}/tags", queryOptions);
+            string url = _tagQueryBuilder.Build(TagsBaseUrl(projectId), queryOptions);
             return await _httpFacade.GetPagedList<Tag>(url);
         }
 
         public async Task<Tag> CreateAsync(CreateTagRequest request) =>
-            await _httpFacade.Post<Tag>($"projects/{request.ProjectId}/repository/tags", request);
+            await _httpFacade.Post<Tag>(TagsBaseUrl(request.ProjectId), request);
 
         public async Task DeleteAsync(DeleteTagRequest request) =>
-            await _httpFacade.Delete($"projects/{request.ProjectId}/repository/tags/{request.TagName}");
+            await _httpFacade.Delete(TagsBaseUrl(request.ProjectId) + "/{request.TagName}");
 
+        public static string TagsBaseUrl(string projectId)
+        {
+            string baseUrl = $"projects/{projectId}/repository/tags";
+            return baseUrl;
+        }
     }
 }
