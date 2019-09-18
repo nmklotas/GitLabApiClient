fabca1f4ca44538c7ef6531f73569b8673004764
David Nagl <dnagl.dev@gmail.com>
2019-09-18 07:58:47 +0200

    Fix JSON deserialization issue (#53)
    * Added ClosedBy clas
    
    * Changed ClosedBy type from string to ClosedBy class

---
+++ src/GitLabApiClient/Models/Issues/Responses/ClosedBy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace GitLabApiClient.Models.Issues.Responses
+{
+    public sealed class ClosedBy : ModifiableObject
+    {
+        [JsonProperty("active")]
+        public string State;
+
+        [JsonProperty("web_url")]
+        public string WebUrl;
+
+        [JsonProperty("avatar_url")]
+        public string AvatarUrl;
+
+        [JsonProperty("username")]
+        public string Username;
+
+        [JsonProperty("id")]
+        public string Id;
+
+        [JsonProperty("name")]
+        public string Name;
+    }
+}
