2a99481dbd751452b21357e2eb51aecadf9489af
Joseph Petersen <josephp90@gmail.com>
2019-09-23 06:51:35 +0200

    xml-doc it, take two

--- src/GitLabApiClient/TagClient.cs
+++ src/GitLabApiClient/TagClient.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using GitLabApiClient.Internal.Http;
 using GitLabApiClient.Internal.Queries;
 using GitLabApiClient.Internal.Utilities;
+using GitLabApiClient.Models.Projects.Responses;
 using GitLabApiClient.Models.Tags.Requests;
 using GitLabApiClient.Models.Tags.Responses;
 
@@ -22,9 +23,21 @@ namespace GitLabApiClient
             _tagQueryBuilder = tagQueryBuilder;
         }
 
+        /// <summary>
+        /// Retrieves a tag by its name
+        /// </summary>
+        /// <param name="projectId">The ID, path or <see cref="Project"/> of the project.</param>
+        /// <param name="tagName">The tag, you want to retrieve.</param>
+        /// <returns></returns>
         public async Task<Tag> GetAsync(object projectId, string tagName) =>
             await _httpFacade.Get<Tag>($"{TagsBaseUrl(projectId)}/{tagName}");
 
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="projectId">The ID, path or <see cref="Project"/> of the project.</param>
+        /// <param name="options">Query options for tags <see cref="TagQueryOptions"/></param>
+        /// <returns></returns>
         public async Task<IList<Tag>> GetAsync(object projectId, Action<TagQueryOptions> options = null)
         {
             var queryOptions = new TagQueryOptions();
@@ -34,9 +47,20 @@ namespace GitLabApiClient
             return await _httpFacade.GetPagedList<Tag>(url);
         }
 
+        /// <summary>
+        /// Create new tag
+        /// </summary>
+        /// <param name="projectId">The ID, path or <see cref="Project"/> of the project.</param>
+        /// <param name="request">Create Tag request.</param>
+        /// <returns>newly created Tag</returns>
         public async Task<Tag> CreateAsync(object projectId, CreateTagRequest request) =>
             await _httpFacade.Post<Tag>(TagsBaseUrl(projectId), request);
 
+        /// <summary>
+        /// Delete a tag
+        /// </summary>
+        /// <param name="projectId">The ID, path or <see cref="Project"/> of the project.</param>
+        /// <param name="tagName">The tag name, you want to delete.</param>
         public async Task DeleteAsync(object projectId, string tagName) =>
             await _httpFacade.Delete($"{TagsBaseUrl(projectId)}/{tagName}");
 
--- src/GitLabApiClient/UploadsClient.cs
+++ src/GitLabApiClient/UploadsClient.cs
@@ -3,6 +3,7 @@ using GitLabApiClient.Internal.Http;
 using GitLabApiClient.Internal.Utilities;
 using GitLabApiClient.Models.Uploads.Requests;
 using GitLabApiClient.Models.Uploads.Responses;
+using GitLabApiClient.Models.Projects.Responses;
 
 namespace GitLabApiClient
 {
@@ -20,7 +21,7 @@ namespace GitLabApiClient
         /// <summary>
         /// Uploads a file for the provided project.
         /// </summary>
-        /// <param name="projectId">The ID or path of the project.</param>
+        /// <param name="projectId">The ID, path or <see cref="Project"/> of the project.</param>
         /// <param name="uploadRequest">The upload request containing the filename and stream to be uploaded</param>
         /// <returns>A <see cref="Upload"/> object.
         /// Use the <see cref="Upload.Markdown"/> property to place the image in your markdown text.
