27af4ffeb70fd8c5ad6237e3529fc00d5252e8a4
Joseph Petersen <josephp90@gmail.com>
2019-10-12 15:19:53 +0200

    fixup pipeline client

--- src/GitLabApiClient/Models/Pipelines/Requests/DeletePipelineRequest.cs
+++
@@ -1,9 +0,0 @@
-namespace GitLabApiClient.Models.Pipelines.Requests
-{
-    public class DeletePipelineRequest : BasePipelineRequest
-    {
-        public DeletePipelineRequest(string projectId, int pipelineId) : base(projectId, pipelineId)
-        {
-        }
-    }
-}
--- src/GitLabApiClient/Models/Pipelines/Requests/RetryPipelineRequest.cs
+++
@@ -1,9 +0,0 @@
-namespace GitLabApiClient.Models.Pipelines.Requests
-{
-    public sealed class RetryPipelineRequest : BasePipelineRequest
-    {
-        public RetryPipelineRequest(string projectId, int pipelineId) : base(projectId, pipelineId)
-        {
-        }
-    }
-}
