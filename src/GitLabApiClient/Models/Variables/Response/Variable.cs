3d6844b54de28fabd9efa60b8e0d1f865dca504b
tellespallabob <55994524+tellespallabob@users.noreply.github.com>
2019-10-22 07:57:12 +0200

    add project variables support (#72)
    * add project environment variables support
    
    * increase coverage project environment variable
    
    * initialize fields explicitely
    
    * use new projectId and fix tests
    
    
    Co-authored-by: Cristian Ramon Garcia <Cristian.RamonGarcia@vistaprint.com>
    Co-authored-by: Joseph Petersen <josephp90@gmail.com>

---
+++ src/GitLabApiClient/Models/Variables/Response/Variable.cs
@@ -0,0 +1,44 @@
+using Newtonsoft.Json;
+
+namespace GitLabApiClient.Models.Variables.Response
+{
+    public sealed class Variable
+    {
+        /// <summary>
+        /// The type of a variable.
+        /// Available types are: env_var (default) and file
+        /// </summary>
+        [JsonProperty("variable_type")]
+        public string VariableType { get; set; }
+
+        /// <summary>
+        /// The key of a variable
+        /// </summary>
+        [JsonProperty("key")]
+        public string Key { get; set; }
+
+        /// <summary>
+        /// The value of a variable
+        /// </summary>
+        [JsonProperty("value")]
+        public string Value { get; set; }
+
+        /// <summary>
+        /// Whether the variable is protected
+        /// </summary>
+        [JsonProperty("protected")]
+        public bool Protected { get; set; }
+
+        /// <summary>
+        /// Whether the variable is masked
+        /// </summary>
+        [JsonProperty("masked")]
+        public bool Masked { get; set; }
+
+        /// <summary>
+        /// The environment_scope of the variable
+        /// </summary>
+        [JsonProperty("environment_scope")]
+        public string EnvironmentScope { get; set; }
+    }
+}
