942ddbe632647598357d33c8d19b24c41b5dd104
Joseph Petersen <josephp90@gmail.com>
2019-10-29 23:19:37 +0100

    enable uploading pre releases

--- build.ps1
+++ build.ps1
@@ -21,12 +21,7 @@ function Build()
 
 function Pack()
 {
-	$packageVersion = "$env:APPVEYOR_REPO_TAG_NAME"
-	if (!$packageVersion) {
-		return
-	}
-
-	& dotnet pack src\GitLabApiClient\GitLabApiClient.csproj -c Release --no-build -p:PackageVersion=$packageVersion | Write-Host
+	& dotnet pack src\GitLabApiClient\GitLabApiClient.csproj -c Release --no-build | Write-Host
 	if ($LastExitCode -ne 0)
 	{
 		exit 1
