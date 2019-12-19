21a3e718b0751331cdb5f1e0ca015721cc545846
Joseph Petersen <josephp90@gmail.com>
2019-12-19 06:57:01 +0100

    switch to GitHub action for Linux CI

--- test.ps1
+++ test.ps1
@@ -1,10 +1,18 @@
-& dotnet test --filter Category!=LinuxIntegration --settings coverletArgs.runsettings --no-build --no-restore
-
-if ($LastExitCode -ne 0)
-{
-	exit 1
+$dockerOSType = docker info --format "{{.OSType}}"
+if ($dockerOSType -ieq 'linux') {
+  & dotnet test --verbosity normal --settings coverletArgs.runsettings --no-build --no-restore
+} else {
+  & dotnet test --filter Category!=LinuxIntegration --verbosity normal --settings coverletArgs.runsettings --no-build --no-restore
 }
 
-Write-Host "Uploading coverage file"
-$openCoverFile = (Get-ChildItem -Path "test/*/coverage.opencover.xml" -Recurse | Sort-Object LastWriteTime | Select-Object -last 1).FullName
-& codecov -f "$openCoverFile" -t bda1c835-c4a2-4a1a-8d38-999b9a8ea80b
+$exitWithError = $LastExitCode -ne 0
+
+$openCoverFile = Get-ChildItem -Path "test/*/coverage.opencover.xml" -Recurse | Sort-Object LastWriteTime | Select-Object -last 1
+if (Test-Path "$openCoverFile") {
+  Write-Host "Uploading coverage file"
+  & codecov -f "$openCoverFile" -t bda1c835-c4a2-4a1a-8d38-999b9a8ea80b
+}
+
+if ($exitWithError) {
+  exit 1
+}
