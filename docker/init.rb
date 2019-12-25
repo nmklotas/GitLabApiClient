f787ee74082ac426c162424e76d12bff83b6867f
Joseph Petersen <josephp90@gmail.com>
2019-12-25 14:49:07 +0100

    fix project/group membership tests

--- docker/init.rb
+++ docker/init.rb
@@ -2,8 +2,10 @@
 u = User.first
 
 g = Group.create!(name: 'txxxestgrouxxxp', path: 'txxxestgrouxxxp')
+g.add_developer(u)
 p = Project.create!(namespace: g, creator: u, path: 'txxxestprojecxxxt', name: 'txxxestprojecxxxt')
 p.repository.create_if_not_exists
+p.add_maintainer(u)
 c = p.repository.create_dir(u, 'newdir', message: 'Create newdir', branch_name: 'master')
 
 t = PersonalAccessToken.new({ user: u, name: 'gitlab-api-client', scopes: ['api']})
