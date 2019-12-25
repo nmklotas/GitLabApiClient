c5193b847e995420a502d1cfc4cb978a39f04104
Joseph Petersen <josephp90@gmail.com>
2019-12-25 20:14:45 +0100

    fix release api and following tests

--- test/GitLabApiClient.Test/Utilities/GitLabApiHelper.cs
+++ test/GitLabApiClient.Test/Utilities/GitLabApiHelper.cs
@@ -29,10 +29,6 @@ namespace GitLabApiClient.Test.Utilities
 
         public static string TestName { get; set; } = "Administrator";
 
-        public static string TestPassword { get; set; } = "hariseldon";
-
-        public static string TestRelease { get; set; } = "release-test";
-        public static string TestTagName { get; set; } = "tag/test";
         public static string TestDescription { get; set; } = "This is just a test-description";
     }
 }
