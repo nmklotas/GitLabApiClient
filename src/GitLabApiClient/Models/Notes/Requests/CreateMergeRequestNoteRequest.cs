15d4adfb8702f8b05c4a93db3dcce89e573c8d30
saber-wang <45062099+saber-wang@users.noreply.github.com>
2020-05-30 21:23:51 +0800

    Add API for get single merge  request, create note on merge request, get pipelines for merge request, delete for webhooks (#137)
    Co-Authored-By: Joseph Petersen <josephp90@gmail.com>

---
+++ src/GitLabApiClient/Models/Notes/Requests/CreateMergeRequestNoteRequest.cs
@@ -0,0 +1,34 @@
+using System;
+using GitLabApiClient.Internal.Utilities;
+using Newtonsoft.Json;
+
+namespace GitLabApiClient.Models.Notes.Requests
+{
+    /// <summary>
+    /// Used to create issue notes in a project.
+    /// </summary>
+    public sealed class CreateMergeRequestNoteRequest
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CreateMergeRequestNoteRequest "/> class.
+        /// </summary>
+        /// <param name="body">The content of a note.</param>
+        public CreateMergeRequestNoteRequest(string body) => Body = body;
+
+        public CreateMergeRequestNoteRequest()
+        {
+        }
+
+        /// <summary>
+        /// The content of a note.
+        /// </summary>
+        [JsonProperty("body")]
+        public string Body { get; set; }
+
+        /// <summary>
+        /// Date time string, ISO 8601 formatted, e.g. 2016-03-11T03:45:40Z (requires admin or project/group owner rights)
+        /// </summary>
+        [JsonProperty("created_at")]
+        public DateTime? CreatedAt { get; set; }
+    }
+}
